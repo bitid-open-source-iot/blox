adfb738246730934630e4429927ec425f1b53cb1
claytoncc <clayton@openiot.co.za>
2021-10-01 12:49:09 +0200

    Added Gauge

--- projects/blox/src/lib/utilities/blox-row.ts
+++ projects/blox/src/lib/utilities/blox-row.ts
@@ -1,3 +1,12 @@
+import { BloxBlank } from './blox-blank';
+import { BloxChart } from './blox-chart';
+import { BloxDefault } from './blox-default';
+import { BloxGauge } from './blox-gauge';
+import { BloxMap } from './blox-map';
+import { BloxTable } from './blox-table';
+import { BloxText } from './blox-text';
+import { BloxValue } from './blox-value';
+import { BloxVector } from './blox-vector';
 import { ObjectId } from './id';
 
 export class BloxRow {
@@ -10,7 +19,30 @@ export class BloxRow {
     constructor(bloxrow?: BLOXROW) {
         if (typeof(bloxrow) != 'undefined' && bloxrow != null) {
             if (Array.isArray(bloxrow.columns)) {
-                this.columns = bloxrow.columns;
+                this.columns = bloxrow.columns.map(o => {
+                    switch(o.type) {
+                        case('map'):
+                            return new BloxMap(o);
+                        case('text'):
+                            return new BloxText(o);
+                        case('table'):
+                            return new BloxTable(o);
+                        case('value'):
+                            return new BloxValue(o);
+                        case('blank'):
+                            return new BloxBlank(o);
+                        case('chart'):
+                            return new BloxChart(o);
+                        case('gauge'):
+                            return new BloxGauge(o);
+                        case('vector'):
+                            return new BloxVector(o);
+                        case('default'):
+                            return new BloxDefault(o);
+                        default:
+                            return null;
+                    };
+                }).filter(o => typeof(o) != 'undefined' && o != null);
             }
             if (typeof(bloxrow.id) != 'undefined' && bloxrow.id != null) {
                 this.id = bloxrow.id;
