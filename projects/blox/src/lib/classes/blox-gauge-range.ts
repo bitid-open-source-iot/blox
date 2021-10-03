d6f53b7a1cbdcec2c9994109ac9d41664e7cd1cd
claytoncc <clayton@openiot.co.za>
2021-10-03 11:07:25 +0200

    changed range id

--- projects/blox/src/lib/classes/blox-gauge-range.ts
+++ projects/blox/src/lib/classes/blox-gauge-range.ts
@@ -2,16 +2,16 @@ import { ObjectId } from './id';
 
 export class BloxGaugeRange {
 
-    public id: string = ObjectId();
-    public end: number = 0;
-    public color: string;
-    public start: number = 0;
+    public end?: number = 0;
+    public color?: string = '#000000';
+    public start?: number = 0;
+    public rangeId?: string = ObjectId();
     public opacity?: number = 100;
 
     constructor(args?: BLOX_GAUGE_RANGE) {
         if (typeof (args) != 'undefined' && args != null) {
-            if (typeof (args.id) != 'undefined' && args.id != null) {
-                this.id = args.id;
+            if (typeof (args.rangeId) != 'undefined' && args.rangeId != null) {
+                this.rangeId = args.rangeId;
             };
             if (typeof (args.end) != 'undefined' && args.end != null) {
                 this.end = args.end;
@@ -31,9 +31,9 @@ export class BloxGaugeRange {
 }
 
 export interface BLOX_GAUGE_RANGE {
-    id?: string;
     end?: number;
     color?: string;
     start?: number;
+    rangeId?: string;
     opacity?: number;
 }
