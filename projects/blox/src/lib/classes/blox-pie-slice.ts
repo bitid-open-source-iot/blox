f9db7de25e312d50cc23292d8d2cbca7e7534ee2
claytoncc <clayton@openiot.co.za>
2021-10-24 17:31:09 +0200

    0.6.5 - Fixed slices

--- projects/blox/src/lib/classes/blox-pie-slice.ts
+++ projects/blox/src/lib/classes/blox-pie-slice.ts
@@ -1,23 +1,17 @@
 export class BloxPieSlice {
 
-    public keys = <{
-        value?: string;
-    }>{
-            value: null
-        };
     public color?: string = '#FFFFFF';
+    public label?: string = null;
     public value?: number = 0;
     public opacity?: number = 100;
 
     constructor(args?: BLOX_PIE_SLICE) {
         if (typeof (args) != 'undefined' && args != null) {
-            if (typeof (args.keys) != 'undefined' && args.keys != null) {
-                if (typeof (args.keys.value) != 'undefined' && args.keys.value != null) {
-                    this.keys.value = args.keys.value;
-                }
-            }
             if (typeof (args.color) != 'undefined' && args.color != null) {
                 this.color = args.color;
+            }
+            if (typeof (args.label) != 'undefined' && args.label != null) {
+                this.label = args.label;
             }
             if (typeof (args.value) != 'undefined' && args.value != null) {
                 this.value = args.value;
@@ -31,10 +25,8 @@ export class BloxPieSlice {
 }
 
 export interface BLOX_PIE_SLICE {
-    keys?: {
-        value?: string;
-    };
     color?: string;
+    label?: string;
     value?: number;
     opacity?: number;
 }
--- projects/blox/src/lib/classes/blox-pie.ts
+++ projects/blox/src/lib/classes/blox-pie.ts
@@ -3,12 +3,27 @@ import { BloxPieSlice } from "./blox-pie-slice";
 export class BloxPie {
 
     readonly type: string = 'pie';
-    
+
+    public keys = <{
+        value?: string;
+        label?: string;
+    }>{
+            value: null,
+            label: null
+        };
     public slices?: BloxPieSlice[] = [];
     public connector?: Object = new Object();
 
     constructor(args?: BLOX_PIE) {
         if (typeof (args) != 'undefined' && args != null) {
+            if (typeof (args.keys) != 'undefined' && args.keys != null) {
+                if (typeof (args.keys.value) != 'undefined' && args.keys.value != null) {
+                    this.keys.value = args.keys.value;
+                }
+                if (typeof (args.keys.label) != 'undefined' && args.keys.label != null) {
+                    this.keys.label = args.keys.label;
+                }
+            }
             if (typeof (args.slices) != 'undefined' && args.slices != null) {
                 this.slices = args.slices.map(o => new BloxPieSlice(o));
             }
@@ -21,6 +36,10 @@ export class BloxPie {
 }
 
 export interface BLOX_PIE {
+    keys?: {
+        value?: string;
+        label?: string;
+    };
     slices?: BloxPieSlice[];
     connector?: Object;
 }
